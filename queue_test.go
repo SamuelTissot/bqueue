d3d5240c65be335054991d8adcf18d78558de19a
Steven Hartland <steven.hartland@multiplay.co.uk>
2021-11-15 13:25:04 +0000

    refactor: queuing logic to increasing performance
    Refactor the queuing logic to:
    * Eliminate unneeded global public mutex, which invalidate API
      boundaries.
    * Eliminate multiple channel actions.
    * Eliminate dedicated QuitChan in favour of closing the jobs channel.
    * Eliminate exposed channels, which exposed internal logic making it
      subject to external breakage.
    * Use channel ranging instead of for select loop.
    * Add configurable options for:
      * Worker count
      * Worker type: dynamic or static
      * Limit for queued jobs
      * Logger
    
    Also:
    * Replace magic 128 with constant, detailing its function.
    * Add tests and benchmark.
    * Add blocking and non blocking queue methods.
    * Add Stop method for shutting down the queue.
    * Eliminate error output, which is a breaking change as the job
      interface is now Process() not Process() error.
    
    Using static workers this reduces the amount of time to process a job
    from ~500ns to ~130ns.

--- queue_test.go
+++ queue_test.go
@@ -1,86 +1,143 @@
 package bqueue
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
+	"context"
+	"io/ioutil"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-type aJob struct {
-	Name string
+type testJob struct {
+	processed bool
 }
 
-func (a aJob) Process() error {
-	if a.Name != "" {
-		log.Printf("test job: %s\n", a.Name)
-		return nil
+func (j *testJob) Process() {
+	j.processed = true
+}
+
+func discardLogger() Option {
+	return Log(log.New(ioutil.Discard, "", 0))
+}
+
+func testQueue(t *testing.T, expectErr bool, options ...Option) {
+	t.Helper()
+
+	q, err := New(options...)
+	if expectErr {
+		require.Error(t, err)
+		return
+	}
+	require.NoError(t, err)
+
+	jobs := make([]*testJob, 5)
+	for i := range jobs {
+		j := &testJob{}
+		jobs[i] = j
+		q.Queue(j)
 	}
 
-	return errors.New("could not print name")
-}
+	err = q.Stop(context.Background())
+	require.NoError(t, err)
 
-func Test_Collect_and_process_job(t *testing.T) {
-	q := New(1)
-	q.Start()
-	j := aJob{"Boo Foo"}
-
-	output := captureStout(func() {
-		q.CollectJob(j)
-	})
-
-	assert.Contains(t, output, "test job: Boo Foo", "The two words should be the same.")
-
-}
-
-func Test_WG(t *testing.T) {
-	q := New(1)
-	q.Start()
-
-	//var output string
-	var output string
-	output += captureStout(func() {
-		for i := 0; i <= 200; i++ {
-			j := aJob{strconv.Itoa(i)}
-			q.CollectJob(j)
-
-		}
-		WG.Wait()
-	})
-
-	// look for all instances of "test job: N"
-	// not very efficient but minimizes false positive
-	for i := 0; i <= 200; i++ {
-		if !strings.Contains(output, fmt.Sprintf("test job: %d", i)) {
-			t.FailNow()
-		}
+	for _, j := range jobs {
+		require.True(t, j.processed)
 	}
 }
 
-func captureStout(f func()) string {
-	var buf bytes.Buffer
-	output := ""
-	log.SetOutput(&buf)
-	start := time.Now()
-	f()
-	for {
-		elapse := time.Since(start).Seconds()
-		if elapse > float64(2) {
-			break
-		}
-		output += buf.String()
-		if output != "" {
-			break
-		}
+func TestOptions(t *testing.T) {
+	cases := []struct {
+		name    string
+		options []Option
+		err     bool
+	}{
+		{
+			name:    "logger",
+			options: []Option{discardLogger()},
+		},
+		{
+			name:    "valid-static-workers",
+			options: []Option{Static(), Workers(2), discardLogger()},
+		},
+		{
+			name:    "negative-static-workers",
+			options: []Option{Static(), Workers(-2), discardLogger()},
+			err:     true,
+		},
+		{
+			name:    "zero-static-workers",
+			options: []Option{Static(), Workers(0), discardLogger()},
+			err:     true,
+		},
+		{
+			name:    "unlimited-static-workers",
+			options: []Option{Static(), Workers(UnlimitedWorkers), discardLogger()},
+			err:     true,
+		},
+		{
+			name:    "valid-dynamic-workers",
+			options: []Option{Workers(2), discardLogger()},
+		},
+		{
+			name:    "negative-dynamic-workers",
+			options: []Option{Workers(-2), discardLogger()},
+			err:     true,
+		},
+		{
+			name:    "zero-dynamic-workers",
+			options: []Option{Workers(0), discardLogger()},
+			err:     true,
+		},
+		{
+			name:    "unlimited-dynamic-workers",
+			options: []Option{Workers(UnlimitedWorkers), discardLogger()},
+		},
+		{
+			name:    "valid-limit",
+			options: []Option{Limit(2), discardLogger()},
+		},
+		{
+			name:    "invalid-limit",
+			options: []Option{Limit(-2), discardLogger()},
+			err:     true,
+		},
 	}
 
-	log.SetOutput(os.Stderr)
-	return output
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			testQueue(t, tc.err, tc.options...)
+		})
+	}
+}
+
+type testJobBlocking struct {
+}
+
+func (j *testJobBlocking) Process() {
+	<-time.After(time.Second)
+}
+
+func TestStop(t *testing.T) {
+	q, err := New(discardLogger())
+	require.NoError(t, err)
+	q.Queue(&testJobBlocking{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	err = q.Stop(ctx)
+	require.Error(t, err)
+}
+
+func BenchmarkQueue(b *testing.B) {
+	q, err := New(Workers(10), Static(), discardLogger())
+	require.NoError(b, err)
+
+	for n := 0; n < b.N; n++ {
+		q.Queue(&testJob{})
+	}
+
+	err = q.Stop(context.Background())
+	require.NoError(b, err)
 }
